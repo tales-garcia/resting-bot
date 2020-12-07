595e2bf8ada6aa1df09c7c43287203f506f6f5d2
Tales Garcia <garcia.tales@gmail.com>
2020-12-07 17:52:09 -0300

    feat(command handling): handle command
    create separated file function to format value and separate action from url param, if body exists,
    it is parsed and passed into handleCommand function, which executes a switch of all actions. Throws
    custom error class

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -1,5 +1,6 @@
-import * as Discord from 'discord.js';
 import config from '../../config/auth';
+import AppError from '../errors/AppError';
+import formatCommand from '../utils/formatCommand';
 
 export default class MessagesController {
 
@@ -10,10 +11,47 @@ export default class MessagesController {
             return MessagesController._printHelp();
         }
 
+        const [action, url] = formatCommand(command);
+        const body = command.split('\`\`\`')[1];
+
+        if(body) {
+            const parsedBody = JSON.parse(body) as object;
+            MessagesController._handleCommand(action, url, parsedBody);
+        } else {
+            MessagesController._handleCommand(action, url);
+        }
+
         return command;
     }
 
+    static _handleCommand(action: string, url: string, body?: object) {
+
+        switch (action) {
+            case 'get': {
+                break;
+            }
+            case 'post': {
+                if(!body) {
+                    throw new AppError(`The command ${action} requires a body in \`JSON\` format`)
+                }
+                break;
+            }
+            case 'put': {
+                if(!body) {
+                    throw new AppError(`The command ${action} requires a body in \`JSON\` format`)
+                }
+                break;
+            }
+            case 'delete': {
+                break;
+            }
+            default: {
+                throw new AppError(`Invalid command: *${action}*`);
+            }
+        }
+    }
+
     static _printHelp() : string {
-        return '> ***List of commands***\n> - **get** <url>                         Send a GET request to <url>\n> - **post** <url>                       Send a POST reques to <url>\n> - **put** <url>                         Send a PUT request to <url>\n> - **delete** <url>                    Send a DELETE request to <url>';
+        return '> ***List of commands***\n> - **get** <url>                         Send a GET request to <url>\n> - **post** <url> <body>        Send a POST reques to <url>\n> - **put** <url> <body>          Send a PUT request to <url>\n> - **delete** <url>                    Send a DELETE request to <url>';
     }
 }
---
+++ src/utils/formatCommand.ts
@@ -0,0 +1,12 @@
+import AppError from "../errors/AppError";
+
+export default function formatCommand(command: string): string[] {
+    command = command.replace(/^\s+/g, '');
+    const [action, url] = command.split(' ');
+
+    if (!url) {
+        throw new AppError(`The command ${action} requires an URL`);
+    }
+
+    return [action, url];
+}
