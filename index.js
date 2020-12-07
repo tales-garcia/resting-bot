5e54dbdb2bcf52466b42dab9f34a19a1a0ce7b66
Tales Garcia <garcia.tales@gmail.com>
2020-12-07 15:05:13 -0300

    feat(message handling): handle message update

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ client.on('message', msg => {
     msg.channel.send(command);
 });
 
+client.on('messageUpdate', msg => {
+    const updatedContent = msg.channel.messages.cache.array()[0].content;
+    
+    if (!updatedContent.startsWith(prefix) || msg.author.bot) return;
+
+    const [, command] = updatedContent.split(prefix);
+
+    if(!command) {
+        printHelp(msg);
+        return;
+    }
+
+    msg.channel.send(command);
+});
+
 function printHelp(msg) {
     msg.channel.send('help')
 }
