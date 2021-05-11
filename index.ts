a290b8a7fe1b5e08db7817c4959ad988a0978cbf
Tales Garcia <garcia.tales@gmail.com>
2021-05-10 21:51:14 -0300

    feat(index,ts): register each bot commands on slash commands

--- index.ts
+++ index.ts
@@ -1,11 +1,32 @@
 import 'dotenv/config';
 import * as Discord from 'discord.js';
 import events from './src/config/events';
+import commands from './src/config/commands';
 
 const client = new Discord.Client();
 
-client.once('ready', () => {
+client.once('ready', async () => {
     console.log('Ready');
+
+    Object.keys(commands).forEach(key => {
+        const command = commands[key]
+
+        const options = command.requires.map(param => ({
+            name: param,
+            description: 'ef',
+            type: 3,
+            required: true
+        }))
+
+        client.api.applications(client.user.id).commands.post({data:{
+            name: key,
+            description: command.description,
+            options
+        }});
+    });
+
+    client.api.applications(client.user.id).commands.set([]);
+
 });
 
 Object.keys(events).forEach(key => {
